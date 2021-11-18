a7873934d996c82de1e5e75c3d080c3f59d1fa0e
Flavio Mendes <fhenrique609@gmail.com>
2021-11-18 17:30:42 -0300

    Crud Documentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls.base import reverse 
+
 from apps.funcionarios.models import Funcionario
 
 # Create your models here.
@@ -6,7 +8,10 @@ from apps.funcionarios.models import Funcionario
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
     pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+    arquivo = models.FileField(upload_to='documentos')
 
     def __str__(self):
         return self.descricao
-    
+
+    def get_absolute_url(self):
+        return reverse('update_funcionario', args=[self.pertence.id])
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -12,6 +12,7 @@ class Funcionario(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+    imagem = models.ImageField()
 
     def __str__(self):
         return self.nome
