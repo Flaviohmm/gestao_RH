a0f0815fef0fa911b6ad1c1da4471e75cd1dba23
Flavio Mendes <fhenrique609@gmail.com>
2021-11-22 16:41:17 -0300

    Diferentes redirecionamentos após salvar hora extra

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,6 +1,6 @@
 from .models import RegistroHoraExtra
 from .forms import RegistroHoraExtraForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     ListView,
     UpdateView,
@@ -24,6 +24,19 @@ class HoraExtraEdit(UpdateView):
 
     def get_form_kwargs(self):
         kwargs = super(HoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({'user' : self.request.user})
+        return kwargs
+
+
+class HoraExtraEditBase(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_success_url(self):
+        return reverse('update_hora_extra_base', args=[self.object.id])
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEditBase, self).get_form_kwargs()
         kwargs.update({'user' : self.request.user})
         return kwargs
 
