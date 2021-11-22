2beb2734291885b7133e109e5b25d388fc73cac4
Flavio Mendes <fhenrique609@gmail.com>
2021-11-22 11:29:25 -0300

    Settings.py 7

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-w+y-dx@!hq0p_8vx0zwo3bmcw=+=t#csv&kc%uv^p2x($$4*m$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.228.166.147', '127.0.0.1']
+ALLOWED_HOSTS = [
+    '18.228.166.147', 
+    '127.0.0.1', 
+    'ec2-18-228-166-147.sa-east-1.compute.amazonaws.com'
+]
 
 
 # Application definition
