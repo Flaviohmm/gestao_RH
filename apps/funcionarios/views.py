0571628d49a1f32377807588a8d168560a47e2e2
Flavio Mendes <fhenrique609@gmail.com>
2021-11-24 09:38:04 -0300

    Relatório de funcionários em PDF com reportlab

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,4 +1,7 @@
+import io
+
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView, 
@@ -7,6 +10,8 @@ from django.views.generic import (
     CreateView
 )
 from .models import Funcionario
+
+from reportlab.pdfgen import canvas
 
 # Create your views here.
 
@@ -40,3 +45,39 @@ class FuncionarioNovo(CreateView):
         funcionario.user = User.objects.create(username=username)
         funcionario.save()
         return super(FuncionarioNovo, self).form_valid(form)
+
+
+def relatorio_funcionarios(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(200, 810, 'Relatório de Funcionarios')
+
+    funcionarios = Funcionario.objects.filter(
+        empresa=request.user.funcionario.empresa
+    )
+
+    str_ = 'Nome: %s | Hora Extra: %.2f'
+
+    p.drawString(0, 800, '_' * 150)
+
+    y = 750
+
+    for funcionario in funcionarios:
+        p.drawString(10, y, str_ % (
+            funcionario.nome, 
+            funcionario.total_horas_extra
+        ))
+        y -= 20
+    
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
